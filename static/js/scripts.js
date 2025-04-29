539e1a635f91a484d2d328815aaa3cbdb4f05b5e
kevin n <kevinudp@163.com>
2025-04-29 10:48:31 +0800

    Update scripts.js

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,6 +1,6 @@
 const content_dir = 'contents/'
 const config_file = 'config.yml'
-const section_names = ['home'];
+const section_names = ['home','publication'];
 
 
 window.addEventListener('DOMContentLoaded', event => {
